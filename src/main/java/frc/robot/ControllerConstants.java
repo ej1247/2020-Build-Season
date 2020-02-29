35ac8bf94ef4e1c1f4236b24f0220b6895884246
Gabe <finkegabriel@gmail.com>
2020-02-29 11:16:28 -0700

    Controller Constants

---
+++ src/main/java/frc/robot/ControllerConstants.java
@@ -0,0 +1,38 @@
+package frc.robot;
+
+public class ControllerConstants{
+
+public final int Xboxcontroller = 0;
+public int Xchannel = 1;
+public int Ychannel = 4;
+public int Ltrigger = 2;
+public int Rtrigger = 3;
+public int Abutton = 1;
+public int Bbutton = 2;
+public int Xbutton = 3;
+public int Ybutton = 4;
+public int LeftBumperbutton = 5;
+public int RightBumperbutton = 6;
+public int Backbutton = 7;
+public int Startbutton = 8;
+public int LeftStickTogglebutton = 9;
+public int RightStickTogglebutton = 10;
+
+//this is for the 3DextremeProLogitech
+public final int flightStick = 1;
+public int flightStickXchannel = 1;
+public int flightStickYchannel = 4;
+public int Zchannel = 2;
+public int Slider = 3;
+public int button1 = 1;
+public int button2 = 2;
+public int button3 = 3;
+public int button4 = 4;
+public int button5 = 5;
+public int button6 = 6;
+public int button7 = 7;
+public int button8 = 8;
+public int button9 = 9;
+public int button10 = 10;
+
+}
